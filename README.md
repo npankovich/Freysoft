2d7b43b157c38495a0b05551b2d50c27ac17066c
Nataliia Pankovich <nataliia.pankovych.fmc@gmail.com>
2021-05-27 13:35:48 +0300

    webpack

---
+++ README.md
@@ -0,0 +1,8 @@
+Building and running on localhost
+
+npm install
+npm run build
+npm run start
+
+
+
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import jQuery from "jquery";
+
+
+jQuery(document).ready(function () {
+
+
+
+
+
+
+});

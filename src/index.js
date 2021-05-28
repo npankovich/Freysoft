1761f1e37c0086227e94aeaec468026b3b2cf4af
Nataliia Pankovich <nataliia.pankovych.fmc@gmail.com>
2021-05-28 14:45:46 +0300

    slider / switch device

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-import "./styles.css";
 import "./scss/style.scss";
 import jQuery from "jquery";
+import "./slider/slick.min.js";
 
 
 jQuery(document).ready(function () {
 
+   jQuery('.gallery-slider').slick({
+    arrows: false,
+    adaptiveHeight: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 150,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive:[{
+        breakpoint: 1440,
+        settings: {
+            slidesToShow: 3,
+        },
+        breakpoint: 320,
+        settings: {
+            slidesToShow: 2,
+        }
+    }],
+    mobileFirst: true
+    // variableWidth: true
 
 
 
+
+   });
+
+//    jQuery('.gallery-slider').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.slider-nav'
+//   });
+//   jQuery('.slider-nav').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     asNavFor: '.slider-for',
+//     dots: true,
+//     centerMode: true,
+//     focusOnSelect: true
+//   });
 
 
 });

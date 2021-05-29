0495cd524a7fed104f551bbbfd6eaf2b484c7eef
Nataliia Pankovich <nataliia.pankovych.fmc@gmail.com>
2021-05-29 23:53:20 +0300

    test project/didnt do second slide,not enough time

--- src/index.js
+++ src/index.js
@@ -25,10 +25,16 @@ jQuery(document).ready(function () {
     }],
     mobileFirst: true,
     variableWidth: true
+   });
 
-
-
-
+   jQuery('.gallery-slider__second').slick({
+    arrows: false,
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 150,
+    mobileFirst: true,
+    variableWidth: true
    });
 
    
@@ -43,71 +49,23 @@ jQuery(document).ready(function () {
 //     centerMode: true,
 //     focusOnSelect: true,
 //     dots: true,
-//     // variableWidth: true,
-//     responsive:[{
-//         // breakpoint: 768,
-//         // settings: {
-//         //     slidesToShow: 2,
-//         // },
+//     adaptiveHeight: true,
+//     variableWidth: true,
+//     mobileFirst: true,
+//     infinite: true,
+//     responsive:[
+//         {
+//         breakpoint: 768,
+//         settings: {
+//             slidesToShow: 2,
+//         },
+//       },
+//       {
 //         breakpoint: 1440,
 //         settings: 'unslick',
-//     }],
-//     // responsive:[{
-//     //     breakpoint: 1440,
-//     //     settings: 'unslick',
-
-
-//     // }],
-
-//     mobileFirst: true
-
-
-
+//       }
+//     ],
 //    });
 
-
-
-//    jQuery('.gallery-slider').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: false,
-//     fade: true,
-//     asNavFor: '.slider-nav'
-//   });
-//   jQuery('.slider-nav').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     asNavFor: '.slider-for',
-//     dots: true,
-//     centerMode: true,
-//     focusOnSelect: true
-//   });
-
-
-
-
-
-// jQuery(window).on('resize', function() {
-
-//         if (jQuery(window).width() <= 768) {
-//                 jQuery('.freySoft-process__inner').slick({
-//                     arrows: false,
-//                     slidesToShow: 2,
-//                     slidesToScroll: 1,
-//                     speed: 150,
-//                     centerMode: true,
-//                     focusOnSelect: true,
-//                     dots: true,
-//                     responsive:[{
-//                         breakpoint: 768,
-//                         settings: {
-//                             slidesToShow: 1,
-//                         },
-//                         // breakpoint: 1440,
-//                         // settings: 'unslick'
-//                     }],
-//                 });
-//         } 
-// });
 
 });

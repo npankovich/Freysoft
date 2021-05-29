00a1c9dbe341f61df94b81635efb855250da2f34
Nata <dreamstore@MacBook-ProNatka.local>
2021-05-29 16:51:34 +0300

    switch d

--- src/index.js
+++ src/index.js
@@ -5,62 +5,65 @@ import "./slider/slick.min.js";
 
 jQuery(document).ready(function () {
 
-//    jQuery('.gallery-slider').slick({
-//     arrows: false,
-//     adaptiveHeight: true,
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     speed: 150,
-//     centerMode: true,
-//     focusOnSelect: true,
-//     responsive:[{
-//         breakpoint: 1440,
-//         settings: {
-//             slidesToShow: 3,
-//         },
-//         breakpoint: 320,
-//         settings: {
-//             slidesToShow: 2,
-//         }
-//     }],
-//     mobileFirst: true
-//     variableWidth: true
-
-
-
-
-//    });
-
-
-jQuery('.freySoft-process__inner').slick({
+   jQuery('.gallery-slider').slick({
     arrows: false,
-    slidesToShow: 1,
+    adaptiveHeight: true,
+    slidesToShow: 4,
     slidesToScroll: 1,
     speed: 150,
     centerMode: true,
     focusOnSelect: true,
-    dots: true,
-    // variableWidth: true,
     responsive:[{
-        // breakpoint: 768,
-        // settings: {
-        //     slidesToShow: 2,
-        // },
         breakpoint: 1440,
-        settings: 'unslick',
+        settings: {
+            slidesToShow: 3,
+        },
+        breakpoint: 320,
+        settings: {
+            slidesToShow: 2,
+        }
     }],
-    // responsive:[{
-    //     breakpoint: 1440,
-    //     settings: 'unslick',
+    mobileFirst: true,
+    variableWidth: true
 
-
-    // }],
-
-    mobileFirst: true
 
 
 
    });
+
+   
+   
+
+
+// jQuery('.freySoft-process__inner').slick({
+//     arrows: false,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     speed: 150,
+//     centerMode: true,
+//     focusOnSelect: true,
+//     dots: true,
+//     // variableWidth: true,
+//     responsive:[{
+//         // breakpoint: 768,
+//         // settings: {
+//         //     slidesToShow: 2,
+//         // },
+//         breakpoint: 1440,
+//         settings: 'unslick',
+//     }],
+//     // responsive:[{
+//     //     breakpoint: 1440,
+//     //     settings: 'unslick',
+
+
+//     // }],
+
+//     mobileFirst: true
+
+
+
+//    });
 
 
 

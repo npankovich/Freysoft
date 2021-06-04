be1b83749d1bd87bbe43798c84c897a047b25122
Nataliia Pankovich <nataliia.pankovych.fmc@gmail.com>
2021-06-04 22:38:22 +0300

    fixed

--- README.md
+++ README.md
@@ -6,5 +6,3 @@ npm run start
 
 
 
-"# DOOB" 
-"# DOOB" 
--- src/index.js
+++ src/index.js
@@ -5,29 +5,67 @@ import "./slider/slick.min.js";
 
 jQuery(document).ready(function () {
 
-//    jQuery('.').slick({
+   jQuery('.gallery-slider').slick({
+    arrows: false,
+    adaptiveHeight: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 150,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive:[{
+        breakpoint: 1440,
+        settings: {
+            slidesToShow: 3,
+        },
+        breakpoint: 320,
+        settings: {
+            slidesToShow: 2,
+        }
+    }],
+    mobileFirst: true,
+    variableWidth: true
+   });
+
+   jQuery('.gallery-slider__second').slick({
+    arrows: false,
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 150,
+    mobileFirst: true,
+    variableWidth: true
+   });
+
+   
+   
+
+
+// jQuery('.freySoft-process__inner').slick({
 //     arrows: false,
-//     adaptiveHeight: true,
-//     slidesToShow: 4,
+//     slidesToShow: 1,
 //     slidesToScroll: 1,
 //     speed: 150,
 //     centerMode: true,
 //     focusOnSelect: true,
-//     responsive:[{
-//         breakpoint: 1440,
-//         settings: {
-//             slidesToShow: 3,
-//         },
-//         breakpoint: 320,
+//     dots: true,
+//     adaptiveHeight: true,
+//     variableWidth: true,
+//     mobileFirst: true,
+//     infinite: true,
+//     responsive:[
+//         {
+//         breakpoint: 768,
 //         settings: {
 //             slidesToShow: 2,
-//         }
-//     }],
-//     mobileFirst: true,
-//     variableWidth: true
+//         },
+//       },
+//       {
+//         breakpoint: 1440,
+//         settings: 'unslick',
+//       }
+//     ],
 //    });
-
-
 
 
 });
